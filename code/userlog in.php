6d0a6bd845eb1af5c6362c7a10524585664db5eb
AlbertMinnah <141026622+AlbertMinnah@users.noreply.github.com>
2023-07-31 12:40:49 +0000

    update

---
+++ code/userlog in.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Food Ordering System</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="userlog in.css">
+</head>
+
+<form class="login-form">
+  <h2>Login</h2>
+  <label for="username">Username:</label>
+  <input type="text" id="username" name="username" required>
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password" required>
+  <button type="submit">Log In</button>
+  <button type="button" id="signup-button">Sign Up</button>
+</form>
+
+
